b6c0315a11e98ff94b6aa7c63be512260ca06190
Jordan Roberts <jordanroberts333@icloud.com>
2025-01-22 02:42:44 +0000

    feat: detect terminal size and adjust text dynamically

--- main.go
+++ main.go
@@ -1,10 +1,45 @@
 package main
 
 import (
-	"fmt"
+	"errors"
+	"log"
+	"main/screens"
+	"os"
+	"unicode"
+
+	"golang.org/x/term"
 )
 
-func main() {
-	fmt.Print("hello world")
+func validateInput(input string) error {
+	for _, char := range input {
+		if !unicode.IsLetter(char) || unicode.IsUpper(char) {
+			return errors.New("string is invalid")
+		}
+	}
+	return nil
 }
 
+
+func main() {
+	width, height, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		log.Fatalf("Failed to get terminal size: %v", err)
+	}
+
+	screens.IntroScreen(height, width)
+	screens.SelectKeys()
+	screens.MonkeyTyping()
+	// 	err := validateInput(input)
+	// 	if err != nil {
+	// 		fmt.Println("Error:", err)
+	// 		fmt.Println("Please try again.")
+	// 	} else {
+	// 		break
+	// 	}
+	// }
+
+	// seed := int64(42) // Your desired seed value
+  // rng := rand.New(rand.NewSource(seed))
+	// randomLetter := rune(rng.Intn(26) + 'a')
+	// fmt.Printf("Random lowercase letter: %c\n", randomLetter)
+}
--- screens/intro.go
+++ screens/intro.go
@@ -7,25 +7,32 @@ import (
 
 	"github.com/fatih/color"
 	"github.com/inancgumus/screen"
+	"github.com/mitchellh/go-wordwrap"
 )
 
-func IntroScreen() {
-	screen.Clear()
-	screen.MoveTopLeft()
+func IntroScreen(height int, width int) {
+	if height >= 9 {
+		screen.Clear()
+		screen.MoveTopLeft()
+	
+		display := color.New(color.FgWhite, color.BgBlack, color.Bold)
+		boldText := color.New(color.Bold)
+		display.Print(" Monkey ")
 
-	display := color.New(color.FgWhite, color.BgGreen, color.Bold)
-	boldText := color.New(color.Bold)
-	display.Print(" Monkey ")
-
-	boldText.Println("  \"Infinite monkey theorem\"")
-	fmt.Println()
-	fmt.Println("\"If an immortal monkey were to hit keys on a typewriter at random for an infinite amount of time, \n it would almost surely type every possible finite sequence of text, including the complete works \n of Shakespeare, every book ever written, and even your own life story.\"")
-	fmt.Println()
-	fmt.Println("∘ Given infinite time, any sequence of events with a non-zero probability will occur.")
-	fmt.Println()
-	fmt.Println("∘ While the probability of typing any specific text (like a novel) is astronomically small, over \n  infinite time, the sheer number of attempts ensures that it will eventually happen.")
-	fmt.Println()
-	fmt.Print("Press enter to continue...")
-	reader := bufio.NewReader(os.Stdin)
-	_, _ = reader.ReadByte()
+		text := `"If an immortal monkey were to hit keys on a typewriter at random for an infinite amount of time, it would almost surely type every possible finite sequence of text, including the complete works of Shakespeare, every book ever written, and even your own life story."`
+		scaledWidth := int(float64(width) * 0.7)
+		wrapped := wordwrap.WrapString(text, uint(scaledWidth))
+		
+		boldText.Println("  \"Infinite monkey theorem\"")
+		fmt.Println()
+		fmt.Println(wrapped)
+		fmt.Println()
+		fmt.Println("∘ Given infinite time, any sequence of events with a non-zero probability will occur.")
+		fmt.Println()
+		fmt.Println("∘ While the probability of typing any specific text (like a novel) is astronomically small, over \n  infinite time, the sheer number of attempts ensures that it will eventually happen.")
+		fmt.Println()
+		fmt.Print("Press enter to continue...")
+		reader := bufio.NewReader(os.Stdin)
+		_, _ = reader.ReadByte()
+	}
 }
